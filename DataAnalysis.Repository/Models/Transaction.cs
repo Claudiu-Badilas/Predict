90b89172ff081e54714408210fcbbfd65cab73b4
claudiu_badilas <Claudiu.Badilas@napa.fi>
2022-12-08 21:16:18 +0200

    add transaction repo

---
+++ DataAnalysis.Repository/Models/Transaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAnalysis.Repository.Models {
+    public class Transaction {
+        public Guid Id { get; set; }
+        public DateTime? RegistrationDate { get; set; }
+        public DateTime? CompletionDate { get; set; }
+        public double? Amount { get; set; }
+        public double? Fee { get; set; }
+        public double? Currency { get; set; }
+        public string Description { get; set; }
+        public double? TransactionType { get; set; }
+        public double? Status { get; set; }
+        public double? Provider { get; set; }
+        public int UserId { get; set; }
+    }
+
+    enum CurrencyType {  EUR = 1, USD = 2, RON = 3 }
+
+    enum TransactionType { 
+        SPEND = 1, RECEIVED = 2, INTERNAL_TRANSFER = 3, TOP_UP = 4, TRANSFER = 5, FEE = 6, CARD_PAYMENT = 7, ATM = 8, EXCHANGE = 9, REWARD = 10, REFUND = 11 
+    }
+    
+    enum TransactionStatus { COMPLETED = 1, PENDING = 2 }
+    
+    enum Provider { RAIFFEISEN = 1, REVOLUT = 2, ORANGE_MONEY = 3 }
+}
---
+++ DataAnalysis.Repository/Repositories/TransactionRepo.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using DataAnalysis.Configuration;
+using DataAnalysis.Repository.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace DataAnalysis.Repository.Repositories {
+    public class TransactionRepo : ITransactionRepo {
+        private readonly NpgsqlDbConnection _conn;
+
+        public TransactionRepo(NpgsqlDbConnection conn) {
+            _conn = conn;
+        }
+
+        public void InsertTransactions(Transaction transactions) {
+            using (var connection = _conn.Connect()) {
+                connection.Open();
+                var sql = @" ";
+                return (await connection.ExecuteScalarAsync(sql, new { transactions }));
+            };
+        }
+    }
+}
