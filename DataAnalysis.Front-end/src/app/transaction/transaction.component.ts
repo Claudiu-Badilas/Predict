e5dddf8417653fe3fa7decd3ef4aea06ae3b96a1
Claudiu <claudiu.badilas@gmail.com>
2023-03-12 13:05:40 +0200

    add component for visualization of the tranzactions

---
+++ DataAnalysis.Front-end/src/app/transaction/transaction.component.ts
@@ -0,0 +1,45 @@
+import { TransactionService } from './transaction.service';
+import { Component, OnInit } from '@angular/core';
+import { first, Observable, of } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  standalone: true,
+  selector: 'app-transaction',
+  template: `<table>
+    <tr *ngFor="let tran of transactions">
+      {{
+        tran.registrationDate
+      }}
+      |{{
+        tran.amount
+      }}
+      |
+      {{
+        tran.description
+      }}
+    </tr>
+  </table>`,
+  styles: [],
+  imports: [CommonModule, HttpClientModule],
+  providers: [TransactionService],
+})
+export class TransactionComponent implements OnInit {
+  transactions: any[] = [];
+
+  ngOnInit(): void {}
+
+  constructor(private _transactionService: TransactionService) {
+    this._transactionService
+      .getTransactions()
+      //.pipe(first())
+      .subscribe((a) => {
+        console.log(
+          '🚀 ~ file: transaction.component.ts:32 ~ TransactionComponent ~ .subscribe ~ a:',
+          a
+        );
+        this.transactions = a;
+      });
+  }
+}
--- DataAnalysis.Front-end/src/main.ts
+++ DataAnalysis.Front-end/src/main.ts
@@ -2,7 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Route } from '@angular/router';
 import { AppComponent } from './app/app.component';
 
-const appRoutes: Route[] = [];
+const appRoutes: Route[] = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./app/transaction/transaction.component').then(
+        (m) => m.TransactionComponent
+      ),
+  },
+];
 
 bootstrapApplication(AppComponent, {
   providers: [provideRouter(appRoutes)],
