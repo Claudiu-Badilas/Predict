3323b8ae49ab6d7b297f63a6c38fd63ef384637f
Claudiu-Badilas <71487855+Claudiu-Badilas@users.noreply.github.com>
2025-12-10 15:38:09 +0200

    Add toogle selection (#31)
    * add comp
    
    * call

--- DataAnalysis.Front-end/src/app/modules/mortgage-module/mortgage.module.ts
+++ DataAnalysis.Front-end/src/app/modules/mortgage-module/mortgage.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DatePickerComponent } from 'src/app/shared/components/date-picker/date-picker.component';
 import { SideBarModule } from 'src/app/shared/components/side-bar/side-bar.module';
+import { ToggleButtonComponent } from 'src/app/shared/components/toggle-button/toggle-button.component';
 import { MortgageRoutingModule } from './mortgage.routing';
 import { MortgageComponent } from './mortgage/mortgage.component';
 
@@ -13,6 +14,7 @@ import { MortgageComponent } from './mortgage/mortgage.component';
     NgbModule,
     SideBarModule,
     DatePickerComponent,
+    ToggleButtonComponent,
   ],
   declarations: [MortgageComponent],
   exports: [MortgageComponent],
