f5629894a9abf2dcf2ae978d5a79a7e8104e6b67
claudiu.badilas <claudiu.badilas@gmail.com>
2025-12-10 16:06:52 +0200

    update redirect

--- DataAnalysis.Front-end/src/app/modules/mortgage-module/mortgage-overview/mortgage-overview.component.ts
+++ DataAnalysis.Front-end/src/app/modules/mortgage-module/mortgage-overview/mortgage-overview.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Store } from '@ngrx/store';
 import { first } from 'rxjs';
 import { DatePickerComponent } from 'src/app/shared/components/date-picker/date-picker.component';
 import { SideBarModule } from 'src/app/shared/components/side-bar/side-bar.module';
 import { ToggleButtonComponent } from 'src/app/shared/components/toggle-button/toggle-button.component';
+import * as fromAppStore from 'src/app/store/app-state.reducer';
+import * as NavigationAction from 'src/app/store/navigation-state/navigation.actions';
 import { GraficRambursare, Rata } from '../models/mortgage.model';
 import { MortgageService } from '../services/mortgage.service';
 
@@ -24,7 +27,10 @@ export class MortgageOverviewComponent {
   transactions: GraficRambursare[] = [];
   state = 'Overview';
 
-  constructor(private readonly _mortgageService: MortgageService) {
+  constructor(
+    private store: Store<fromAppStore.AppState>,
+    private readonly _mortgageService: MortgageService
+  ) {
     this._mortgageService
       .getMortgages()
       .pipe(first())
@@ -37,6 +43,14 @@ export class MortgageOverviewComponent {
         ];
         // this.rateSelectate = res[0].rate.slice(0, 7);
       });
+  }
+
+  onSelectionChange(module: string) {
+    this.store.dispatch(
+      NavigationAction.navigateTo({
+        route: `/mortgage/${module.toLocaleLowerCase()}`,
+      })
+    );
   }
 
   rateSelectate: Rata[] = [];
