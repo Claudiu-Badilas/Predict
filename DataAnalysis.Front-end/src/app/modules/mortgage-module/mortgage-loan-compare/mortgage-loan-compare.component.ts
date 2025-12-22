9185b7121e8e238fe8f7dcc44502b41e178647af
claudiu.badilas <claudiu.badilas@gmail.com>
2025-12-22 22:36:21 +0200

    add selection

--- DataAnalysis.Front-end/src/app/modules/mortgage-module/mortgage-loan-compare/mortgage-loan-compare.component.ts
+++ DataAnalysis.Front-end/src/app/modules/mortgage-module/mortgage-loan-compare/mortgage-loan-compare.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { BehaviorSubject, combineLatest, map } from 'rxjs';
+import * as fromMortgageLoan from 'src/app/modules/mortgage-module/state-management/mortgage-loan.reducer';
+import { DropdownSelectComponent } from 'src/app/shared/components/dropdown-select/dropdown-select.component';
 import { SideBarModule } from 'src/app/shared/components/side-bar/side-bar.module';
 import { ToggleButtonComponent } from 'src/app/shared/components/toggle-button/toggle-button.component';
 import * as fromAppStore from 'src/app/store/app-state.reducer';
 import * as NavigationAction from 'src/app/store/navigation-state/navigation.actions';
-import { RepaymentSchedule } from '../models/mortgage.model';
+
 import { MortgageLoanCompareBodyComponent } from './components/mortgage-loan-compare-body/mortgage-loan-compare-body.component';
-import * as fromMortgageLoan from 'src/app/modules/mortgage-module/state-management/mortgage-loan.reducer';
 
 @Component({
   selector: 'app-mortgage-loan-compare',
@@ -16,14 +18,33 @@ import * as fromMortgageLoan from 'src/app/modules/mortgage-module/state-managem
     SideBarModule,
     ToggleButtonComponent,
     MortgageLoanCompareBodyComponent,
+    DropdownSelectComponent,
   ],
   templateUrl: './mortgage-loan-compare.component.html',
-  styleUrl: './mortgage-loan-compare.component.scss',
+  styleUrls: ['./mortgage-loan-compare.component.scss'],
 })
 export class MortgageLoanCompareComponent {
   repaymentSchedules$ = this.store.select(
     fromMortgageLoan.getRepaymentSchedules
   );
+
+  repaymentSchedulesOptions$ = this.repaymentSchedules$.pipe(
+    map((rs) => rs.map((r) => r.name))
+  );
+
+  selectedLeftValue$ = new BehaviorSubject<string>('No Selection');
+  selectedRightValue$ = new BehaviorSubject<string>('No Selection');
+
+  leftRepaymentSchedules$ = combineLatest([
+    this.repaymentSchedules$,
+    this.selectedLeftValue$,
+  ]).pipe(map(([rs, selected]) => rs.find((r) => r.name === selected)));
+
+  rightRepaymentSchedules$ = combineLatest([
+    this.repaymentSchedules$,
+    this.selectedRightValue$,
+  ]).pipe(map(([rs, selected]) => rs.find((r) => r.name === selected)));
+
   constructor(private store: Store<fromAppStore.AppState>) {}
 
   onSelectionChange(module: string) {
@@ -32,5 +53,13 @@ export class MortgageLoanCompareComponent {
         route: `/mortgage-loan/${module.toLowerCase()}`,
       })
     );
+  }
+
+  onLeftDropdownSelected(event: string) {
+    this.selectedLeftValue$.next(event);
+  }
+
+  onRightDropdownSelected(event: string) {
+    this.selectedRightValue$.next(event);
   }
 }
