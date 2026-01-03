5cbe111308490eccccc5673d9e783d1d30c6f070
claudiu.badilas <claudiu.badilas@gmail.com>
2026-01-03 19:47:14 +0200

    update routing

--- Predict.Front-end/src/app/modules/mortgage-module/mortgage-loan.routing.ts
+++ Predict.Front-end/src/app/modules/mortgage-module/mortgage-loan.routing.ts
@@ -1,36 +1,27 @@
-import { ModuleWithProviders } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MortgageLoanCompareComponent } from './mortgage-loan-compare/mortgage-loan-compare.component';
 import { MortgageLoanDetailedComponent } from './mortgage-loan-detailed/mortgage-loan-detailed.component';
 import { MortgageLoanOverviewComponent } from './mortgage-loan-overview/mortgage-loan-overview.component';
-import { MortgageLoanComponent } from './mortgage-loan.component';
-import { MortgageLoanModule } from './mortgage-loan.module';
 import { MortgageLoanSimulatorComponent } from './mortgage-loan-simulator/mortgage-loan-simulator.component';
+import { MortgageLoanComponent } from './mortgage-loan.component';
 
 const routes: Routes = [
   {
-    path: 'mortgage-loan',
+    path: '',
     component: MortgageLoanComponent,
     children: [
-      {
-        path: 'overview',
-        component: MortgageLoanOverviewComponent,
-      },
-      {
-        path: 'compare',
-        component: MortgageLoanCompareComponent,
-      },
-      {
-        path: 'detailed',
-        component: MortgageLoanDetailedComponent,
-      },
-      {
-        path: 'simulator',
-        component: MortgageLoanSimulatorComponent,
-      },
+      { path: 'overview', component: MortgageLoanOverviewComponent },
+      { path: 'compare', component: MortgageLoanCompareComponent },
+      { path: 'detailed', component: MortgageLoanDetailedComponent },
+      { path: 'simulator', component: MortgageLoanSimulatorComponent },
+      { path: '', redirectTo: 'overview', pathMatch: 'full' },
     ],
   },
 ];
 
-export const MortgageLoanRoutingModule: ModuleWithProviders<MortgageLoanModule> =
-  RouterModule.forChild(routes);
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class MortgageLoanRoutingModule {}
