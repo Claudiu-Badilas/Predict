92bab886268a330253066b0f550b34d9350a2e07
claudiu.badilas <claudiu.badilas@gmail.com>
2026-02-07 15:27:28 +0200

    update header

--- Predict.Client/src/app/modules/mortgage-loan/mortgage-loan-compare/components/mortgage-loan-compare-header/mortgage-loan-compare-header.component.ts
+++ Predict.Client/src/app/modules/mortgage-loan/mortgage-loan-compare/components/mortgage-loan-compare-header/mortgage-loan-compare-header.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { Store } from '@ngrx/store';
+import * as fromMortgageLoanCompare from 'src/app/modules/mortgage-loan/mortgage-loan-compare/selectors/mortgage-loan-compare.selectors';
+import * as fromMortgageLoan from 'src/app/modules/mortgage-loan/reducers/mortgage-loan.reducer';
+import { NumberFormatPipe } from 'src/app/shared/pipes/number-format.pipe';
 
 @Component({
   selector: 'app-mortgage-loan-compare-header',
-  imports: [],
+  imports: [CommonModule, NumberFormatPipe],
   templateUrl: './mortgage-loan-compare-header.component.html',
   styleUrl: './mortgage-loan-compare-header.component.scss',
 })
 export class MortgageLoanCompareHeaderComponent {
+  private readonly store = inject(Store<fromMortgageLoan.MortgageLoanState>);
 
+  baseHistocialInstalmentPaymentBatchesManager = toSignal(
+    this.store.select(
+      fromMortgageLoanCompare.getBaseHistocialInstalmentPaymentBatchesManager,
+    ),
+  );
+  leftHistocialInstalmentPaymentBatchesManager = toSignal(
+    this.store.select(
+      fromMortgageLoanCompare.getLeftHistocialInstalmentPaymentBatchesManager,
+    ),
+  );
+  rightHistocialInstalmentPaymentBatchesManager = toSignal(
+    this.store.select(
+      fromMortgageLoanCompare.getRightHistocialInstalmentPaymentBatchesManager,
+    ),
+  );
 }
