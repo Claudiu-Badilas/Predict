f55354de3890b6e87f97c67d66d448cb71652542
claudiu.badilas <claudiu.badilas@gmail.com>
2026-02-18 21:54:52 +0200

    add db infra proj

---
+++ Predict.Database/Program.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+var connectionString = "host=localhost;username=postgres;password=admin;database=predict";
+
+var services = new ServiceCollection()
+    .AddFluentMigratorCore()
+    .ConfigureRunner(rb => rb
+        .AddPostgres()
+        .WithGlobalConnectionString(connectionString)
+        .ScanIn(typeof(Program).Assembly).For.Migrations())
+    .AddLogging(lb => lb.AddFluentMigratorConsole())
+    .BuildServiceProvider();
+
+using var scope = services.CreateScope();
+var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+
+runner.MigrateUp();
