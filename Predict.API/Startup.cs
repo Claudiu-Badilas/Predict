cfacbb23aff02166947f6e97d0cfd9b92cabbf94
claudiu.badilas <claudiu.badilas@gmail.com>
2026-02-19 23:45:41 +0200

    add swagger

--- Predict.API/Startup.cs
+++ Predict.API/Startup.cs
@@ -11,12 +11,12 @@ using Predict.Service.AuthorizationService;
 using Predict.Service.CacheService;
 using Predict.Service.CacheServicel;
 using Predict.Service.TokenService;
+using Microsoft.OpenApi;
 
 namespace Predict;
 
 public class Startup(IConfiguration config)
 {
-
     public IConfiguration Configuration { get; }
 
     public void ConfigureServices(IServiceCollection services)
@@ -43,6 +43,43 @@ public class Startup(IConfiguration config)
         services.AddSingleton<IEnvironmentConfiguration, EnvironmentConfiguration>();
 
         services.AddIdentityServices(config, new EnvironmentConfiguration());
+
+        // Swagger
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Predict API",
+                Version = "v1"
+            });
+
+            // Add JWT Bearer support in Swagger UI
+            //options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            //{
+            //    Name = "Authorization",
+            //    Type = SecuritySchemeType.ApiKey,
+            //    Scheme = "Bearer",
+            //    BearerFormat = "JWT",
+            //    In = ParameterLocation.Header,
+            //    Description = "Enter: Bearer {your token}"
+            //});
+
+            //options.AddSecurityRequirement(new OpenApiSecurityRequirement
+            //{
+            //    {
+            //        new OpenApiSecurityScheme
+            //        {
+            //            Reference = new OpenApiReference
+            //            {
+            //                Type = ReferenceType.SecurityScheme,
+            //                Id = "Bearer"
+            //            }
+            //        },
+            //        Array.Empty<string>()
+            //    }
+            //});
+        });
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -51,19 +88,25 @@ public class Startup(IConfiguration config)
         {
             app.UseDeveloperExceptionPage();
         }
+
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Predict API v1");
+            options.RoutePrefix = "swagger";
+        });
+
         app.UseMiddleware<ExceptionMiddleware>();
 
         app.UseRouting();
 
         app.UseCors(builder =>
-           builder.AllowAnyOrigin()
-               .AllowAnyHeader()
-               .AllowAnyMethod()
-               //.WithOrigins("http://localhost:4200/")
-               );
+            builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+        );
 
         app.UseAuthentication();
-
         app.UseAuthorization();
 
         app.UseEndpoints(endpoints =>
