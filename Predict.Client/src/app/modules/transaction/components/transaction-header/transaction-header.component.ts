c3cff1eed5fe1c76432cbb70b79704ad53c1a0e8
claudiu.badilas <claudiu.badilas@gmail.com>
2026-05-01 14:26:47 +0300

    remove monthly agg

--- Predict.Client/src/app/modules/transaction/components/transaction-header/transaction-header.component.ts
+++ Predict.Client/src/app/modules/transaction/components/transaction-header/transaction-header.component.ts
@@ -1,14 +1,10 @@
 import { Component, computed, input } from '@angular/core';
 import { TransactionDomain } from '../../models/transactions.model';
-import { MonthlyMostCommonTransactionComponent } from '../monthly-most-common-transaction/monthly-most-common-transaction.component';
 import { MostCommonTransactionComponent } from '../most-common-transaction/most-common-transaction.component';
 
 @Component({
   selector: 'p-transaction-header',
-  imports: [
-    MostCommonTransactionComponent,
-    MonthlyMostCommonTransactionComponent,
-  ],
+  imports: [MostCommonTransactionComponent],
   templateUrl: './transaction-header.component.html',
   styleUrls: ['./transaction-header.component.scss'],
 })
